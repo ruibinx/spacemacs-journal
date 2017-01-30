3be6b870b0c1100eefed5de0b9009f67df1d452f
robinx <xingrobin@gmail.com>
2017-01-30 15:01:28 +0800

    add calendar mode bindings

--- packages.el
+++ packages.el
@@ -9,7 +9,11 @@
 ;;
 ;;; License: GPLv3
 
-(defconst journal-packages '(org-journal)
+(defconst journal-packages
+  '(
+    org-journal
+    calendar
+    )
   )
 
 (defun journal//set-global-keys ()
@@ -35,6 +39,18 @@
     "s"  'org-journal-search)
   )
 
+(defun journal//set-calendar-keys ()
+  (define-key calendar-mode-map "Jj" 'org-journal-new-date-entry)
+  (define-key calendar-mode-map "Jv" 'org-journal-read-entry)
+  (define-key calendar-mode-map "JV" 'org-journal-display-entry)
+  (define-key calendar-mode-map "Jn" 'org-journal-next-entry)
+  (define-key calendar-mode-map "Jp" 'org-journal-previous-entry)
+  (define-key calendar-mode-map "Jf" 'org-journal-search-forever)
+  (define-key calendar-mode-map "Jw" 'org-journal-search-calendar-week)
+  (define-key calendar-mode-map "Jm" 'org-journal-search-calendar-month)
+  (define-key calendar-mode-map "Jy" 'org-journal-search-calendar-month)
+  )
+
 (defun journal/init-org-journal ()
   (use-package org-journal
     :defer t
@@ -43,7 +59,17 @@
       (journal//set-global-keys)
       (journal//set-major-mode-keys)
       )
+    )
+  )
 
+(defun journal/init-calendar ()
+  (use-package calendar
+    :config
+    (progn
+      (journal//set-calendar-keys)
+      (add-hook 'calendar-today-visible-hook 'org-journal-mark-entries)
+      (add-hook 'calendar-today-invisible-hook 'org-journal-mark-entries)
+      )
     )
   )
 
