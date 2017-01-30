ba41e7c8c9293aa380dd8d6bff539c7a01f7b3e2
robinx <xingrobin@gmail.com>
2017-01-30 21:02:20 +0800

    add author info

--- packages.el
+++ packages.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
-;; Author:  <robin@robin-laptop>
+;; Author:  robinx <xingrobin@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
